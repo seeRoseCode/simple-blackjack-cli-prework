90db7c519aab61e7d62b5efa2c9f92e1eb5bea0a
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-20 14:17:35 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,17 +1,17 @@
 def welcome
-  # code #welcome here
+  puts"Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total (x)
+  puts "Your cards add up to #{x}"
 end
 
 def prompt_user
-  # code #prompt_user here
+   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
@@ -41,4 +41,3 @@ end
 def runner
   # code runner here
 end
-    
